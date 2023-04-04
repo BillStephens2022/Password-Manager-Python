036ba81628435466adfbbba4ad0cc017eef73a4c
BillStephens2022 <two4onebill@yahoo.com>
2023-04-04 14:26:00 -0400

    set up layout of UI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
 
+
 # TODO:
 # Setup UI
 # Setup Password Generator
@@ -8,13 +9,32 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200, highlightthickness=0) #highlightthickness removes the border around the image
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=0, row=0)
+canvas.grid(column=1, row=0)
 
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+website_input = Entry(width=38)
+website_input.grid(column=1, row=1, columnspan=2)
 
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+email_input = Entry(width=38)
+email_input.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+password_input = Entry(width=21)
+password_input.grid(column=1, row=3)
+
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
