9491d82f34536519a336af6a1703b1c326c1c1d9
BillStephens2022 <two4onebill@yahoo.com>
2023-04-05 09:25:44 -0400

    replaced text file with json file - password data now written in json format to the json file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 
 # Generate Random Password
@@ -27,18 +28,20 @@ def save():
     website = website_input.get()
     email = email_input.get()
     password = password_input.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
-    else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\nEmail: {email}"
-                            f"\nPassword: {password}\nIs it OK to save?")
-        
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_input.delete(0, END)
-                password_input.delete(0, END)
+    else: 
+        with open("data.json", "w") as data_file:
+            json.dump(new_data, data_file, indent=4)
+            website_input.delete(0, END)
+            password_input.delete(0, END)
 
 
 # Setup UI
